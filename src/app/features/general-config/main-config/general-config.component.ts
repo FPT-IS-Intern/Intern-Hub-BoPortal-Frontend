5eef49adee58243dca2f0b3d6b92bb414398d253
vominhdat68 <datminhvo0901@gmail.com>
2026-03-07 16:03:29 +0700

    feat(general-config): integrate GeneralConfigService into component

--- src/app/features/general-config/main-config/general-config.component.ts
+++ src/app/features/general-config/main-config/general-config.component.ts
@@ -1,13 +1,15 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMessageService } from 'ng-zorro-antd/message';
 import { GeneralInfoComponent } from '../general-info/general-info.component';
 import { SystemFormatComponent } from '../system-format/system-format.component';
 import { TimeConfigComponent } from '../time-config/time-config.component';
+import { GeneralConfigService } from '../../../services/general-config.service';
 
 @Component({
   selector: 'app-general-config',
@@ -27,10 +29,14 @@ import { TimeConfigComponent } from '../time-config/time-config.component';
   styleUrl: './general-config.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GeneralConfigComponent {
+export class GeneralConfigComponent implements OnInit {
   protected readonly form: FormGroup;
 
-  constructor(private readonly fb: FormBuilder) {
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly configService: GeneralConfigService,
+    private readonly message: NzMessageService
+  ) {
     this.form = this.fb.group({
       appName: ['', []],
       language: [null as string | null, []],
@@ -40,10 +46,35 @@ export class GeneralConfigComponent {
     });
   }
 
+  ngOnInit(): void {
+    this.fetchConfig();
+  }
+
+  private fetchConfig(): void {
+    this.configService.getConfig().subscribe({
+      next: (res) => {
+        if (res.data) {
+          this.form.patchValue(res.data);
+        }
+      },
+      error: (err) => {
+        console.error('Fetch config error:', err);
+        this.message.error('Không thể tải cấu hình hệ thống');
+      },
+    });
+  }
+
   protected onSubmit(): void {
     if (this.form.valid) {
-      console.log('Apply config:', this.form.value);
-      // TODO: Gọi API lưu cấu hình
+      this.configService.updateConfig(this.form.value).subscribe({
+        next: () => {
+          this.message.success('Cập nhật cấu hình thành công');
+        },
+        error: (err) => {
+          console.error('Update config error:', err);
+          this.message.error('Cập nhật cấu hình thất bại');
+        },
+      });
     }
   }
 }
