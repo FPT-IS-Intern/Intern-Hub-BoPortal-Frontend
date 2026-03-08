d73b887c35d9b1d1e8a0b093f4a66e9ccefc2f4d
vominhdat68 <datminhvo0901@gmail.com>
2026-03-08 16:17:17 +0700

    feat: create SharedInputTextComponent and refactor login to use it

---
+++ src/app/components/shared-input-text/shared-input-text.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+    selector: 'app-shared-input-text',
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './shared-input-text.component.html',
+    styleUrls: ['./shared-input-text.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SharedInputTextComponent {
+    @Input() headerInput = '';
+    @Input() placeholder = '';
+    @Input() typeInput: 'text' | 'password' = 'text';
+    @Input() required = false;
+    @Input() icon: string | null = null;
+    @Input() value = '';
+
+    @Output() valueChange = new EventEmitter<string>();
+    @Output() iconClick = new EventEmitter<void>();
+
+    onValueChange(newValue: string) {
+        this.valueChange.emit(newValue);
+    }
+
+    onIconClick() {
+        this.iconClick.emit();
+    }
+}
