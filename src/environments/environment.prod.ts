cc80c1ee49f6399d10403f09acbbd9a763eff389
Bravos <baonq8205@gmail.com>
2026-03-10 22:26:12 +0700

    Update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://admin-internhub-v2.bbtech.io.vn',
   storageFileBaseUrl: 'https://s3.vn-hcm-1.vietnix.cloud/bravos/',
 };
