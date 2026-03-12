542caac133525b90bb5841db84632079c0f165db
vominhdat68 <datminhvo0901@gmail.com>
2026-03-12 22:55:36 +0700

    feat: create reusable SharedSearchComponent

---
+++ src/app/components/shared-search/shared-search.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+@Component({
+  selector: 'app-shared-search',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NzIconModule],
+  templateUrl: './shared-search.component.html',
+  styleUrls: ['./shared-search.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SharedSearchComponent {
+  @Input() value = '';
+  @Input() placeholder = 'Tìm kiếm...';
+  @Input() width = '280px';
+
+  @Output() valueChange = new EventEmitter<string>();
+
+  onValueChange(newValue: string) {
+    this.valueChange.emit(newValue);
+  }
+}
