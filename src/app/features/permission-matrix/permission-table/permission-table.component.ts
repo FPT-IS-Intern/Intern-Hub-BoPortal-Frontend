c64bbd93f2db557162d073549e9d34257c128eca
vominhdat68 <datminhvo0901@gmail.com>
2026-03-15 11:50:17 +0700

    feat: replace nz-checkbox with custom styled checkbox in permission matrix

--- src/app/features/permission-matrix/permission-table/permission-table.component.ts
+++ src/app/features/permission-matrix/permission-table/permission-table.component.ts
@@ -1,13 +1,13 @@
 import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+
 import { NoDataComponent } from '../../../components/no-data/no-data.component';
 
 @Component({
     selector: 'app-permission-table',
     standalone: true,
-    imports: [CommonModule, FormsModule, NzCheckboxModule, NoDataComponent],
+    imports: [CommonModule, FormsModule, NoDataComponent],
     templateUrl: './permission-table.component.html',
     styleUrl: './permission-table.component.scss',
 })
