f4d9cf9c20986af533bc4f58fb65a848ebde408b
vominhdat68 <datminhvo0901@gmail.com>
2026-03-17 14:38:05 +0700

    feat(checkin): add tabs skeleton

---
+++ src/app/components/skeletons/checkin-tabs-skeleton/checkin-tabs-skeleton.component.ts
@@ -0,0 +1,12 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-checkin-tabs-skeleton',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './checkin-tabs-skeleton.component.html',
+  styleUrl: './checkin-tabs-skeleton.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CheckinTabsSkeletonComponent {}
